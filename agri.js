91196cbc88cbac573487118810cefcaec2819def
AndrewsGilbert <tandrewsgilbert@gmail.com>
2021-06-24 22:00:11 +0530

    3rd change store value & display

--- agri.js
+++ agri.js
@@ -1,54 +1,52 @@
-const prompt1 = require('prompt-sync')();
-const prompt2 = require('prompt-sync')();
-const prompt3 = require('prompt-sync')();
-const prompt4 = require('prompt-sync')();
-const prompt5 = require('prompt-sync')();
-const prompt6 = require('prompt-sync')();
-const prompt7 = require('prompt-sync')();
-const prompt8 = require('prompt-sync')();
-const prompt9 = require('prompt-sync')();
+const prompt1 = require('prompt-sync')()
+const prompt2 = require('prompt-sync')()
+const prompt3 = require('prompt-sync')()
+const prompt4 = require('prompt-sync')()
+const prompt5 = require('prompt-sync')()
+const prompt6 = require('prompt-sync')()
+const prompt7 = require('prompt-sync')()
+const prompt8 = require('prompt-sync')()
+const prompt9 = require('prompt-sync')()
 
- 
-var Survey_number = prompt1('Survey Number ');
-var Length = prompt2('Length ');
-var Width = prompt3('Width ');
-var Location = prompt4('Location ');
-var Owner_name = prompt5('Owner_name ');
-var Contact_No = prompt6('Contact_No ');
-var Soil = prompt7('Soil ');
-var Soil_ph = prompt8('Soil_ph ');
-var Plant = prompt9('Plant ');
+const surveyNumber = prompt1('surveyNumber ')
+const Length = prompt2('Length ')
+const Width = prompt3('Width ')
+const Location = prompt4('Location ')
+const ownerName = prompt5('ownerName ')
+const contactNo = prompt6('contactNo ')
+const Soil = prompt7('Soil ')
+const soilPh = prompt8('soilPh ')
+const Plant = prompt9('Plant ')
 
+const landsDetails = {}
 
-var lands_details = {};
-
-var lands_count = (Object.keys(lands_details).length);
-
-class plot{
-    constructor(Survey_number, Length, Width, Location,Owner_name,Contact_No,Soil,Soil_ph,Plant) {
-        this.plot_id = Survey_number;
-        this.len = Length;
-        this.wid = Width;
-        this.location = Location;
-        this.owner_name = Owner_name;
-        this.contact_no = Contact_No;
-        this.soil = Soil;
-        this.soil_ph = Soil_ph;
-        this.plant = Plant;
-        
-    }
+const landsCount = (Object.keys(landsDetails).length)
+class Plot {
+  constructor (surveyNumber, Length, Width, Location, ownerName, contactNo, Soil, soilPh, Plant) {
+    this.plot_id = surveyNumber
+    this.len = Length
+    this.wid = Width
+    this.location = Location
+    this.owner_name = ownerName
+    this.contact_no = contactNo
+    this.soil = Soil
+    this.soil_ph = soilPh
+    this.plant = Plant
+  }
 };
 
+createLand()
 
+function createLand () {
+  landsDetails[landsCount + 1] = new Plot(surveyNumber, Length, Width, Location, ownerName, contactNo, Soil, soilPh, Plant)
 
-function create_land() {
-
-     lands_details [lands_count+1] = new plot(Survey_number, Length, Width, Location,Owner_name,Contact_No,Soil,Soil_ph,Plant);
-    
-
-    return lands_details;
+  return landsDetails
 };
 
+const fs = require('fs')
 
+fs.appendFileSync('AgricultureData.txt', JSON.stringify(landsDetails, null, 2), 'utf8')
 
-console.log(create_land()); // to register land details
+const read = fs.readFileSync('AgricultureData.txt', 'utf8')
+
+console.log(read)
