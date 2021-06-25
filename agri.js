e3fd6a2c76e564fe2af76911a995ec49ef50744b
AndrewsGilbert <tandrewsgilbert@gmail.com>
2021-06-25 18:13:30 +0530

    5th change

--- agri.js
+++ agri.js
@@ -66,21 +66,63 @@ function changeData () {
 
   const data = fs.readFileSync('store.txt', 'utf8')
   const landsDetails = JSON.parse(data)
-  const landsKeys = (Object.keys(landsDetails[1]))
+  const landsKeys = (Object.keys(landsDetails))
+  const landsChar = (Object.keys(landsDetails[1]))
 
   const id = prompt1(' Id number ')
-  console.log(landsKeys)
-  const property = prompt2(' Which data to be change ')
-  const detail = prompt3(' Enter the data ')
-  const mainKey = landsDetails[id]
-  mainKey[property] = detail
 
-  fs.writeFileSync('store.txt', JSON.stringify(landsDetails, null, 2), 'utf8')
+  if (landsKeys.includes(id)) {
+    console.log(landsChar)
+    const property = prompt2(' Which data to be change ')
 
-  console.log('\r\n' + 'Land details modified')
-  console.log('\r\n', mainKey)
+    if (landsChar.includes(property)) {
+      const detail = prompt3(' Enter the data ')
+      if (detail !== '') {
+        const mainKey = landsDetails[id]
+        mainKey[property] = detail
+
+        fs.writeFileSync('store.txt', JSON.stringify(landsDetails, null, 2), 'utf8')
+      }
+      console.log('\r\n' + 'Land details modified')
+      console.log('\r\n', landsDetails[id])
+    } else { console.log('property doest not exist') }
+  } else { console.log('id does not exist') }
 }
 
-// console.log(createLand())   // to add a land
-// console.log(display())      // to view all the land details
-// console.log(changeData())   // to modify the existing land details
+function findPlant () {
+  const array = []
+  const prompt1 = require('prompt-sync')()
+  const reqPlant = prompt1('Enter the plant name ')
+
+  const data = fs.readFileSync('store.txt', 'utf8')
+  const landsDetails = JSON.parse(data)
+
+  for (const id in landsDetails) {
+    if (landsDetails[id].plant === reqPlant) { array.push(id) }
+  }
+
+  console.log('\r\n' + ' no of plots with ' + reqPlant + ' = ' + array.length) // no of lands with this plant
+
+  console.log('\r\n' + ' plot numbers which is with ' + reqPlant + ' = ' + array) // land id of with this plant is stored
+
+  console.log(getLandDet()) // to get the details of lands,which is having particular plant
+
+  function getLandDet () {
+    console.log('\r\n' + ' The details of land which is having ' + reqPlant + 'are ')
+
+    for (let i = 0; i < array.length; i++) {
+      const ref = i + 1
+      const plantLandDet = landsDetails[array[i]]
+
+      console.log('\r\n' + ' S.N. ' + ref + ' > ', plantLandDet)
+    }
+  }
+}
+
+// console.log(createLand()) // to add a land
+
+// console.log(display()) // to view all the land details
+
+console.log(changeData()) // to modify the existing land details
+
+// console.log(findPlant()) // to find no of lands having particular plant
